6e18a777ed980e40d110df283ba5a0fee18d40c7
Tsai Fu Chung <114430905+Tsai-fu@users.noreply.github.com>
2024-05-30 17:54:45 -0700

    change to sticky navbar; modal fix needed
    javascript of sticky navbar and back-to-top button have conflict, only one can work at time

--- script.js
+++ script.js
@@ -19,3 +19,66 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+// ----------------------------------------------------------------------------
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction2();
+};
+
+// Get the navbar
+var navbar = document.getElementById("nav-bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction2() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("img-to-modal");
+var modalImg = document.getElementById("img01");
+
+document.querySelectorAll(".img-to-modal").forEach((element) => {
+  element.addEventListener("click", () => {
+    modalImg.src = this.src;
+    modal.style.display = "block";
+    // modalImg.src = this.src;
+  });
+});
+
+// ----------------------------------------------------------------------------
+// // Get the modal
+// var modal = document.getElementById("myModal");
+
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImg");
+// var modalImg = document.getElementById("img01");
+
+// // var captionText = document.getElementById("caption");
+// img.onclick = function () {
+//   modal.style.display = "block";
+//   modalImg.src = this.src;
+//   // captionText.innerHTML = this.alt;
+// };
+
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//   modal.style.display = "none";
+// };
+
+// ----------------------------------------------------------------------------
